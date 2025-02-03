a14eb83b77c08ba1dd901d1c624daba5214c56a6
Jake Coffman <jake@jakecoffman.com>
2025-02-02 18:55:57 -0600

    last route

---
+++ src/routes/you/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+	import Hero from "$lib/components/Hero.svelte";
+	const session = "$lib/stores/session";
+
+	async function logout() {
+		await fetch(`/stldevs-api/logout`, {credentials: 'include'})
+		location.reload()
+	}
+	async function optOut() {
+		const r = await fetch(`/stldevs-api/me`, {
+			credentials: 'include',
+			method: 'PATCH',
+			headers: {'content-type': 'application/json'},
+			body: JSON.stringify({Hide: true}),
+		})
+		if (!r.ok) {
+			return alert(await r.text())
+		}
+		const profile = await r.json()
+		session.set({me: profile})
+	}
+	async function optIn() {
+		const r = await fetch(`/stldevs-api/me`, {
+			credentials: 'include',
+			method: 'PATCH',
+			headers: {'content-type': 'application/json'},
+			body: JSON.stringify({Hide: false})
+		})
+		if (!r.ok) {
+			return alert(await r.text())
+		}
+		const profile = await r.json()
+		session.set({me: profile})
+	}
+</script>
+
+<Hero title="You"/>
+
+<article>
+	{#if !session.me}
+		<p>You aren't logged in. You can log in to opt-out of this website.</p>
+
+		<a class="button" href="/stldevs-api/login">Log in with GitHub</a>
+
+		<p>
+			If you'd rather not log in with GitHub, mention me on Twitter and I'll opt you out.
+			<a href="https://twitter.com/nill" target="_blank">@nill</a>
+		</p>
+	{:else}
+		Welcome {session.me.name || session.me.login}!
+		{#if !session.me.hide}
+			<div>
+				<p>To opt out of stldevs click here:</p>
+				<button on:click={optOut}>Opt Out</button>
+			</div>
+		{:else}
+			<div>
+				<p>You should be hidden now.</p>
+				<p>To opt back in to stldevs click here:</p>
+				<button on:click={optIn}>Opt In</button>
+			</div>
+		{/if}
+
+		{#if session.me.is_admin}
+			<p>You're an admin</p>
+		{/if}
+
+		<p>
+			<button on:click={logout}>Logout</button>
+		</p>
+	{/if}
+</article>
