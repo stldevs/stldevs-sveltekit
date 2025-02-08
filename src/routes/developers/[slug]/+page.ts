6df05eb88705418aaacab22a7db6c5faf15534f5
Jake Coffman <jake@jakecoffman.com>
2025-02-08 13:49:57 -0600

    rollback

--- src/routes/developers/[slug]/+page.ts
+++ src/routes/developers/[slug]/+page.ts
@@ -1,15 +1,15 @@
 import type { PageLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ params, url, fetch }) => {
+export const load: PageLoad = async ({ params, fetch }) => {
 	const slug = params.slug;
 
-	const api = new URL(`/stldevs-api/devs/${encodeURIComponent(slug)}`, url.origin);
-	const r = await fetch(api);
+	const r = await fetch(`/stldevs-api/devs/${encodeURIComponent(slug)}`);
 	if (!r.ok) {
 		if (r.status === 404) {
 			return error(404, { message: 'Developer not found' });
 		}
+		console.log('Error fetching', r);
 		return error(500, { message: 'Error fetching developer' });
 	}
 
